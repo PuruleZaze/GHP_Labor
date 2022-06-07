555fe68d8fefaae6ba6a845039e6e8940d042e6c
aeropagz <klaas.pelzer@gmx.de>
2022-06-07 11:17:36 +0200

    add server

---
+++ components/server/mdns.c
@@ -0,0 +1,31 @@
+#include "sdkconfig.h"
+#include "driver/gpio.h"
+#include "esp_netif.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "mdns.h"
+#include "lwip/apps/netbiosns.h"
+
+#include "server.h"
+
+
+#define MDNS_INSTANCE "esp home web server"
+
+static const char *TAG = "example";
+
+esp_err_t start_rest_server(const char *base_path);
+
+static void initialise_mdns(void)
+{
+    mdns_init();
+    mdns_hostname_set(CONFIG_EXAMPLE_MDNS_HOST_NAME);
+    mdns_instance_name_set(MDNS_INSTANCE);
+
+    mdns_txt_item_t serviceTxtData[] = {
+        {"board", "esp32"},
+        {"path", "/"}
+    };
+
+    ESP_ERROR_CHECK(mdns_service_add("ESP32-WebServer", "_http", "_tcp", 80, serviceTxtData,
+                                     sizeof(serviceTxtData) / sizeof(serviceTxtData[0])));
+}
